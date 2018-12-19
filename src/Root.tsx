ecd914e733274aa3dd28a732f29e33fe1d601d92
阿部 悠世 <abe@goga.co.jp>
2018-12-19 17:25:44 +0900

    fix module resolving: absolute

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MyComponent from './components/MyComponent';
+import MyComponent from 'components/MyComponent';
 
 export interface Props {
   name: string
